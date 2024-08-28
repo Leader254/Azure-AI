bda2dfbc7f7087b1eb2ef8b8459c7dade5f9998b
Leader254 <gatheru.samuel@students.kyu.ac.ke>
2024-08-28 12:47:03 +0300

    refactor

--- client/src/actions.js
+++ client/src/actions.js
@@ -13,190 +13,149 @@ export default function LanguageActions() {
   });
 
   const items = [
-    { label: "Detect Language", icon: "pi pi-language" },
-    { label: "Analyze Sentiments", icon: "pi pi-chart-scatter" },
-    { label: "Extract Key Phrases", icon: "pi pi-key" },
-    { label: "Recognize Linked Entities", icon: "pi pi-link" },
+    { label: "Detect Language", icon: "pi pi-language", apiPath: "/detect" },
+    {
+      label: "Analyze Sentiments",
+      icon: "pi pi-chart-scatter",
+      apiPath: "/analyzeSentiment",
+    },
+    { label: "Extract Key Phrases", icon: "pi pi-key", apiPath: "/keyPhrases" },
+    {
+      label: "Recognize Linked Entities",
+      icon: "pi pi-link",
+      apiPath: "/entities",
+    },
   ];
 
   const handleTabChange = async (e) => {
-    const selectedItem = e.value.label;
+    const { apiPath, label } = e.value;
 
-    if (selectedItem === "Detect Language") {
-      try {
-        const response = await axios.get(
-          "http://localhost:5000/api/pdf/detect"
-        );
-        const { extractedText, languageDetection } = response.data;
-        setAnalysisInfo({
-          text: extractedText,
-          languageDetection: languageDetection || [],
-          sentimentAnalysis: null,
-          keyPhrases: null,
-          linkedEntities: null,
-        });
-      } catch (error) {
-        console.error("Error detecting language:", error);
-        setAnalysisInfo({
-          text: null,
-          languageDetection: [
-            { detectedLanguage: "Error", iso6391Code: "", confidenceScore: 0 },
-          ],
-          sentimentAnalysis: null,
-          keyPhrases: null,
-          linkedEntities: null,
-        });
-      }
-    } else if (selectedItem === "Analyze Sentiments") {
-      try {
-        const response = await axios.get(
-          "http://localhost:5000/api/pdf/analyzeSentiment"
-        );
-        setAnalysisInfo({
-          text: null,
-          languageDetection: null,
-          sentimentAnalysis: response.data || [],
-          keyPhrases: null,
-          linkedEntities: null,
-        });
-      } catch (error) {
-        console.error("Error analyzing sentiment:", error);
-        setAnalysisInfo({
-          text: null,
-          languageDetection: null,
-          sentimentAnalysis: [
-            {
-              text: "Error analyzing sentiment.",
-              sentiment: "Error",
-              confidenceScores: {},
-            },
-          ],
-          keyPhrases: null,
-          linkedEntities: null,
-        });
-      }
-    } else if (selectedItem === "Extract Key Phrases") {
-      try {
-        const response = await axios.get(
-          "http://localhost:5000/api/pdf/keyPhrases"
-        );
-        console.log("first", response);
-        setAnalysisInfo({
-          text: null,
-          languageDetection: null,
-          sentimentAnalysis: null,
-          keyPhrases: response.data || [],
-          linkedEntities: null,
-        });
-      } catch (error) {
-        console.error("Error extracting key phrases:", error);
-        setAnalysisInfo({
-          text: null,
-          languageDetection: null,
-          sentimentAnalysis: null,
-          keyPhrases: [
-            { text: "Error extracting key phrases.", keyPhrases: ["Error"] },
-          ],
-          linkedEntities: null,
-        });
-      }
-    } else if (selectedItem === "Recognize Linked Entities") {
-      try {
-        const response = await axios.get(
-          "http://localhost:5000/api/pdf/entities"
-        );
-        console.log("response", response.data.entities);
-        setAnalysisInfo({
-          text: null,
-          languageDetection: null,
-          sentimentAnalysis: null,
-          keyPhrases: null,
-          linkedEntities: response.data.entities || [],
-        });
-        console.log(response.data);
-      } catch (error) {
-        console.error("Error extracting key entites:", error);
-        setAnalysisInfo({
-          text: null,
-          languageDetection: null,
-          sentimentAnalysis: null,
-          keyPhrases: null,
-          linkedEntities: [
-            {
-              text: "Error extracting linked entities.",
-              linkedEntities: ["Error"],
-            },
-          ],
-        });
-      }
+    try {
+      const response = await axios.get(
+        `http://localhost:5000/api/pdf${apiPath}`
+      );
+      const data = response.data;
+
+      setAnalysisInfo((prevInfo) => ({
+        ...prevInfo,
+        text: label === "Detect Language" ? data.extractedText : null,
+        languageDetection:
+          label === "Detect Language" ? data.languageDetection || [] : null,
+        sentimentAnalysis: label === "Analyze Sentiments" ? data || [] : null,
+        keyPhrases: label === "Extract Key Phrases" ? data || [] : null,
+        linkedEntities:
+          label === "Recognize Linked Entities" ? data.entities || [] : null,
+      }));
+    } catch (error) {
+      console.error(`Error in ${label.toLowerCase()}:`, error);
+      setAnalysisInfo((prevInfo) => ({
+        ...prevInfo,
+        text: null,
+        languageDetection:
+          label === "Detect Language"
+            ? [
+                {
+                  detectedLanguage: "Error",
+                  iso6391Code: "",
+                  confidenceScore: 0,
+                },
+              ]
+            : null,
+        sentimentAnalysis:
+          label === "Analyze Sentiments"
+            ? [
+                {
+                  text: "Error analyzing sentiment.",
+                  sentiment: "Error",
+                  confidenceScores: {},
+                },
+              ]
+            : null,
+        keyPhrases:
+          label === "Extract Key Phrases"
+            ? [{ text: "Error extracting key phrases.", keyPhrases: ["Error"] }]
+            : null,
+        linkedEntities:
+          label === "Recognize Linked Entities"
+            ? [
+                {
+                  text: "Error extracting linked entities.",
+                  linkedEntities: ["Error"],
+                },
+              ]
+            : null,
+      }));
     }
   };
+
+  const renderCard = (title, content) => (
+    <Card title={title}>
+      {content || <p className="m-0">No {title.toLowerCase()} available.</p>}
+    </Card>
+  );
 
   return (
     <div>
       <TabMenu model={items} onTabChange={handleTabChange} className="mt-3" />
-
       <div className="card">
-        {analysisInfo.text && (
-          <Card title="Extracted Text">
+        {analysisInfo.text &&
+          renderCard(
+            "Extracted Text",
             <p className="m-0">{analysisInfo.text}</p>
-          </Card>
-        )}
+          )}
 
-        {analysisInfo.languageDetection && (
-          <Card title="Language Detection Result">
-            {analysisInfo.languageDetection.length > 0 ? (
+        {analysisInfo.languageDetection &&
+          renderCard(
+            "Language Detection Result",
+            analysisInfo.languageDetection.length > 0 ? (
               <p className="m-0">
-                <strong>ISO 6391 Code:</strong>{" "}
+                <strong>Language:</strong>
+                {analysisInfo.languageDetection[0].detectedLanguage} <br />
+                <strong>ISO 6391 Code:</strong>
                 {analysisInfo.languageDetection[0].iso6391Code} <br />
-                <strong>Confidence Score:</strong>{" "}
+                <strong>Confidence Score:</strong>
                 {analysisInfo.languageDetection[0].confidenceScore}
               </p>
-            ) : (
-              <p className="m-0">No language detected.</p>
-            )}
-          </Card>
-        )}
+            ) : null
+          )}
 
-        {analysisInfo.sentimentAnalysis && (
-          <Card title="Sentiment Analysis Result">
-            {analysisInfo.sentimentAnalysis.length > 0 ? (
-              analysisInfo.sentimentAnalysis.map((result, index) => (
-                <div key={index}>
-                  <p className="m-0">
-                    <strong>Overall Sentiment:</strong> {result.sentiment}{" "}
-                    <br />
-                    <strong>Confidence Scores:</strong>{" "}
-                    {JSON.stringify(result.confidenceScores)} <br />
-                    <strong>Sentences:</strong>
-                  </p>
-                  {result.sentences.map((sentence, idx) => (
-                    <div key={idx} style={{ marginLeft: "20px" }}>
-                      <p className="m-0">
-                        <strong>Sentence Sentiment:</strong>{" "}
-                        {sentence.sentiment} <br />
-                        <strong>Confidence Scores:</strong>{" "}
-                        {JSON.stringify(sentence.confidenceScores)}
-                      </p>
-                    </div>
-                  ))}
-                </div>
-              ))
-            ) : (
-              <p className="m-0">No sentiment analysis results available.</p>
-            )}
-          </Card>
-        )}
+        {analysisInfo.sentimentAnalysis &&
+          renderCard(
+            "Sentiment Analysis Result",
+            analysisInfo.sentimentAnalysis.length > 0
+              ? analysisInfo.sentimentAnalysis.map((result, index) => (
+                  <div key={index}>
+                    <p className="m-0">
+                      <strong>Overall Sentiment:</strong> {result.sentiment}
+                      <br />
+                      <strong>Confidence Scores:</strong>
+                      {JSON.stringify(result.confidenceScores)} <br />
+                      <strong>Sentences:</strong>
+                    </p>
+                    {result.sentences.map((sentence, idx) => (
+                      <div key={idx} style={{ marginLeft: "20px" }}>
+                        <p className="m-0">
+                          <strong>Sentence Sentiment:</strong>
+                          {sentence.sentiment} <br />
+                          <strong>Confidence Scores:</strong>
+                          {JSON.stringify(sentence.confidenceScores)}
+                        </p>
+                      </div>
+                    ))}
+                  </div>
+                ))
+              : null
+          )}
 
-        {analysisInfo.keyPhrases && (
-          <Card title="Key Phrases Extraction Result">
-            {analysisInfo.keyPhrases.length > 0 ? (
-              analysisInfo.keyPhrases.map((result, index) => (
-                <div key={index}>
-                  <p className="m-0">
-                    <strong>Key Phrases:</strong>
-                  </p>
-                  {result.keyPhrases.length > 0 ? (
+        {analysisInfo.keyPhrases &&
+          renderCard(
+            "Key Phrases Extraction Result",
+            analysisInfo.keyPhrases.length > 0
+              ? analysisInfo.keyPhrases.map((result, index) => (
+                  <div key={index}>
+                    <p className="m-0">
+                      <strong>Key Phrases:</strong>
+                    </p>
                     <ul style={{ listStyleType: "none", paddingLeft: "0" }}>
                       {result.keyPhrases.map((phrase, idx) => (
                         <li key={idx} style={{ paddingBottom: "4px" }}>
@@ -204,20 +163,15 @@ export default function LanguageActions() {
                         </li>
                       ))}
                     </ul>
-                  ) : (
-                    <p>No key phrases extracted.</p>
-                  )}
-                </div>
-              ))
-            ) : (
-              <p className="m-0">No key phrases available.</p>
-            )}
-          </Card>
-        )}
+                  </div>
+                ))
+              : null
+          )}
 
-        {analysisInfo.linkedEntities && (
-          <Card title="Linked Entities Results">
-            {analysisInfo.linkedEntities.length > 0 ? (
+        {analysisInfo.linkedEntities &&
+          renderCard(
+            "Linked Entities Results",
+            analysisInfo.linkedEntities.length > 0 ? (
               <div style={{ display: "flex", flexWrap: "wrap", gap: "16px" }}>
                 {analysisInfo.linkedEntities.map((entity, index) => (
                   <div
@@ -233,19 +187,19 @@ export default function LanguageActions() {
                     <div className="m-0">
                       <strong>Entity:</strong> {entity.name} <br />
                       <strong>Data Source:</strong> {entity.dataSource} <br />
-                      <strong>URL:</strong>{" "}
+                      <strong>URL:</strong>
                       <a
                         href={entity.url}
                         target="_blank"
                         rel="noopener noreferrer"
                       >
-                        {entity.url}
+                        Follow link
                       </a>
                       <br />
                       {entity.matches.map((match, matchIndex) => (
                         <div key={matchIndex}>
                           <strong>Matched Text:</strong> {match.text} <br />
-                          <strong>Confidence Score:</strong>{" "}
+                          <strong>Confidence Score:</strong>
                           {match.confidenceScore}
                         </div>
                       ))}
@@ -253,11 +207,8 @@ export default function LanguageActions() {
                   </div>
                 ))}
               </div>
-            ) : (
-              <div className="m-0">No linked entities detected.</div>
-            )}
-          </Card>
-        )}
+            ) : null
+          )}
       </div>
     </div>
   );
